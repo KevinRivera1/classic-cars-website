a82c1d79da2ed818219323445e5bd7ae4d8e992e
Kevin Rivera <shimpo11.kr@gmail.com>
2025-01-23 20:09:20 -0500

    feat: Configurar Tailwind CSS con Vite y ajustar integraciones en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [sitemap(), tailwind()],
+    integrations: [sitemap()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
