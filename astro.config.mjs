06acdead0b2250dd935a4e2da18197720c37e5ad
Kevin Rivera <shimpo11.kr@gmail.com>
2025-01-29 16:02:33 -0500

    feat: Agregar la URL del sitio en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://classic-cars-website.vercel.app/',
     integrations: [sitemap()],
     vite: {
         plugins: [tailwindcss()],
