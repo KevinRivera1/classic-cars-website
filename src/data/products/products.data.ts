2abd1e43a2ffbd2cc3182a3934611b66b29a3c9a
Kevin Rivera <shimpo11.kr@gmail.com>
2025-01-30 17:53:30 -0500

    feat: Agregar datos de productos con imágenes y descripciones

---
+++ src/data/products/products.data.ts
@@ -0,0 +1,27 @@
+import type { IProduct } from "@/core/domain/products/products.entity";
+import ImageRoyce from "../../assets/car-1.avif";
+import ImageMGB from "../../assets/car-2.avif";
+import ImageFerrari from "../../assets/car-3.avif";
+
+export const productsItems: IProduct[] = [
+  {
+    image: ImageRoyce.src,
+    name: "Rolls-Royce silver",
+    subtitle: "Cloud III Rear",
+    description: "Production: 1955-1966",
+    price: "$ 240.000",
+  },
+  {
+    image: ImageMGB.src,
+    name: "MG MGB",
+    description: "Production: 1962-1980",
+    price: "$ 234.000",
+  },
+  {
+    image: ImageFerrari.src,
+    name: "Ferrari 250 GT LWB",
+    subtitle: "California Spyder",
+    description: "Production: 1957-1963",
+    price: "$ 735.000",
+  },
+];
