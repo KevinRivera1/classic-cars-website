6b890080a1a2feedd22a8c38741dd56ba460867b
Kevin Rivera <shimpo11.kr@gmail.com>
2025-02-01 17:21:56 -0500

    feat: actualizar nombres y enlaces de categorías en los filtros

--- src/data/filters/filter.data.ts
+++ src/data/filters/filter.data.ts
@@ -6,28 +6,27 @@ export const filterItems: IFilter[] = [
     link: "/all",
   },
   {
-    name: "Category 1",
-    link: "/category-1",
+    name: "Browse by Category",
+    link: "/browse-by-category",
     subItems: [
       { name: "Subcategory 1.1", link: "/category-1/sub-1" },
       { name: "Subcategory 1.2", link: "/category-1/sub-2" },
     ],
   },
   {
-    name: "Category 2",
-    link: "/category-2",
+    name: "Brand",
+    link: "/brand",
     subItems: [
       { name: "Subcategory 2.1", link: "/category-2/sub-1" },
       { name: "Subcategory 2.2", link: "/category-2/sub-2" },
     ],
   },
   {
-    name: "Category 3",
-    link: "/category-3",
-  },
-  {
-    name: "Category 4",
-    link: "/category-4",
+    name: "Country",
+    link: "/country",
+    subItems: [
+      { name: "Subcategory 3.1", link: "/country/sub-1" },
+      { name: "Subcategory 3.2", link: "/country/sub-2" },
+    ],
   },
 ];
-
